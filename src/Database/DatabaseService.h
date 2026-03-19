4ac4e80e9170a32437f12007c18d708f7d27f5d6
Gokulakrishnan Sivakumar <S.gokulakrishnan92@gmail.com>
2026-03-19 17:50:39 +0100

    Use smart pointers in database service API

--- src/Database/DatabaseService.h
+++ src/Database/DatabaseService.h
@@ -4,19 +4,24 @@
 
 #include <QList>
 #include <QString>
+#include <memory>
 
 class DatabaseService
 {
 public:
+    using PatientPtr = std::shared_ptr<Patient>;
+    using StudyPtr = std::shared_ptr<Study>;
+    using SeriesPtr = std::shared_ptr<Series>;
+
     virtual ~DatabaseService() = default;
 
-    virtual void savePatient(const Patient& patient) = 0;
-    virtual Patient* getPatient(const QString& patientID) = 0;
-    virtual QList<Patient*> getAllPatients() = 0;
+    virtual void savePatient(const PatientPtr& patient) = 0;
+    virtual PatientPtr getPatient(const QString& patientID) = 0;
+    virtual QList<PatientPtr> getAllPatients() = 0;
 
-    virtual void saveStudy(const Study& study) = 0;
-    virtual Study* getStudy(const QString& studyUID) = 0;
+    virtual void saveStudy(const StudyPtr& study) = 0;
+    virtual StudyPtr getStudy(const QString& studyUID) = 0;
 
-    virtual void saveSeries(const Series& series) = 0;
-    virtual Series* getSeries(const QString& seriesUID) = 0;
+    virtual void saveSeries(const SeriesPtr& series) = 0;
+    virtual SeriesPtr getSeries(const QString& seriesUID) = 0;
 };
